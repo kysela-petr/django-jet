225f506a81e5c87549b47a265f9a363add1662f9
Ramses Martinez <ramses.mtz96@gmail.com>
2020-01-21 23:12:32 -0600

    remove python2 support

--- jet/tests/models.py
+++ jet/tests/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 
 
-@python_2_unicode_compatible
 class TestModel(models.Model):
     field1 = models.CharField(max_length=255)
     field2 = models.IntegerField()
@@ -11,15 +9,12 @@ class TestModel(models.Model):
         return '%s%d' % (self.field1, self.field2)
 
 
-@python_2_unicode_compatible
 class RelatedToTestModel(models.Model):
     field = models.ForeignKey(TestModel, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.field
 
-
-@python_2_unicode_compatible
 class SearchableTestModel(models.Model):
     field1 = models.CharField(max_length=255)
     field2 = models.IntegerField()
